f4df78a9239ae60ad87e12a820be81864675d1ac
fmonthel <fmonthel@flox-arts.net>
2017-02-18 22:10:45 -0500

    [BUG] concatenate std and int

--- cleanup.py
+++ cleanup.py
@@ -13,9 +13,9 @@ def main() :
 	month_to_keep_list_arch = 1
 	month_to_keep_card_arch = 3
 	mongo_user = 'admin'
-	mongo_password = 'password'
+	mongo_password = 'admin123'
 	mongo_server = 'localhost'
-	mongo_port = 27017
+	mongo_port = '27017'
 
 	# Variables
 	time_start = datetime.datetime.now()
